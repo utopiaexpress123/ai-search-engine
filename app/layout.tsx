1f2a4dd09901feb3962e903730194b4056b18f95
rick-sys <ricksanchezy2x@gmail.com>
2024-04-12 22:57:42 +0200

    dd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from 'next';
-import { Inter as FontSans } from "next/font/google";
 import { AI } from './action';
 import './globals.css';
 import { cn } from '@/lib/utils';
@@ -7,10 +6,6 @@ import { ThemeProvider } from '@/components/theme-provider';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 const title = 'Utopia Web Research'
 const description =
@@ -45,12 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
